e91e3bd6595c0b97999ca6afb6c04b5421e4f415
Kimia <kimia.rfp@gmail.com>
2023-04-11 22:36:06 +0330

    add js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // hide signin page when clicked on create page link
+let signinForm = document.querySelector("div.signin-form");
+let signup = document.querySelector(".sign-up");
 
-function hideForm() {
-  let signinForm = document.querySelector("div.signin-form");
-  let signup = document.querySelector(".sign-up");
+function hideLoginForm(event) {
+  event.preventDefault();
   signinForm.style.display = "none";
   signup.style.display = "block";
 }
-let link = document.querySelector("a#create-account-link");
-link.addEventListener("click", hideForm);
+let createLink = document.querySelector("a#create-account-link");
+createLink.addEventListener("click", hideLoginForm);
+
+// hide signup page when clicked on login link
+
+function hideSecondForm(event) {
+  event.preventDefault();
+  signinForm.style.display = "block";
+  signup.style.display = "none";
+}
+let loginLink = document.querySelector("a.additional-link");
+loginLink.addEventListener("click", hideSecondForm);
